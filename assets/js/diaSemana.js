2fa3bb336a78f26bd19ca817fcdbb7cc66c02c7b
Nahuel <nahuelcarlos150@gmail.com>
2024-05-03 09:57:25 -0300

    Se creo el programa diaSemana

---
+++ assets/js/diaSemana.js
@@ -0,0 +1,26 @@
+let dia = prompt("Ingrese un numero de un día de la semana")
+
+if(dia==1){
+    console.log("Su día es lunes")
+    }
+    else if(dia==2){
+        console.log("Su día es martes")
+    }
+    else if(dia==3){
+        console.log("Su día es miercoles")
+    }
+    else if(dia==4){
+        console.log("Su día es jueves")
+    }
+    else if(dia==5){
+        console.log("Su día es viernes")
+    }
+    else if(dia==6){
+        console.log("Su día es sábado")
+    }
+    else if(dia==7){
+        console.log("Su día es domingo")
+    }
+else{
+    console.log("Ingrese un número valido")
+}
