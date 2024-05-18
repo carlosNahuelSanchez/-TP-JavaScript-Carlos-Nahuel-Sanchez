c48d380b9d391bbc0915622adfd286bca1471142
carlosNahuelSanchez <nahuelcarlos150@gmail.com>
2024-05-19 01:27:54 +0200

    Se creo el bucle de opciones hasta que el usuario salga del programa

--- trabajoPractico3/assets/js/app.js
+++ trabajoPractico3/assets/js/app.js
@@ -1,6 +1,28 @@
 let listaTareas = []
+let accionUsuario
 
-    let accionUsuario = parseInt(prompt("Seleccione que quiere hacer 1-Agregar Tarea 2-Listar Tareas 3-Editar Tarea 4-Eliminar Tarea 6-Salir"))
+do {
+    accionUsuario = parseInt(prompt("Seleccione que quiere hacer 1-Agregar Tarea 2-Listar Tareas 3-Editar Tarea 4-Eliminar Tarea  5-Salir"))
     
+    switch (accionUsuario) {
+        case 1:
+            
+        break;
+        case 2:
 
+        break;
+        case 3:
+        
+        break;
+        case 4:
+        
+        break;
+        case 5:
+            alert("Usted ha salido")
+        break;
+        default:
+            alert("Valor no valido")      
+        break;
+    }
+} while (accionUsuario !== 5)
     
