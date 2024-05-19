33ff5c50e70c61c6e379cf70c827b4055f3e521f
carlosNahuelSanchez <nahuelcarlos150@gmail.com>
2024-05-19 22:29:46 +0200

    Se añadieron condiciones a los prompts del usuario

--- trabajoPractico3/assets/js/app.js
+++ trabajoPractico3/assets/js/app.js
@@ -1,51 +1,74 @@
-let listaTareas = []
-let accionUsuario
+let listaTareas = [];
+let accionUsuario;
 
 function tareaAgr(tareaNew) {
-    tareaNew = prompt("¿Que tarea desea agregar?")
-    listaTareas.push(tareaNew)
-    alert("Se añadio " + tareaNew + " a su lista de tareas con exito")
+    tareaNew = prompt("¿Qué tarea desea agregar?");
+    if (tareaNew.trim() !== tareaNew) {
+        alert("ERROR: Evita los espacios al inicio o al final de la tarea.");
+    }
+    else if (tareaNew === "" || tareaNew === null) {
+        alert("ERROR: No puede ingresar un espacio vacío.");
+    }
+    else {
+        listaTareas.push(tareaNew);
+        alert("Se añadió " + tareaNew + " a su lista de tareas con éxito.");
+    }
 }
 
 function tareaEdit(tareaAModificar, iTareaAModificar, tareaModificada) {
-    tareaAModificar = prompt("¿Que tarea desea modificar de la lista? " + listaTareas)
-    iTareaAModificar = listaTareas.indexOf(tareaAModificar)
-    tareaModificada = prompt("¿Cual sera el nuevo nombre de la tarea seleccionada?")
-    listaTareas.splice(iTareaAModificar, 1, tareaModificada)
-    alert("Se modifico correctamente la tarea")
+    tareaAModificar = prompt("¿Qué tarea desea modificar de la lista? " + listaTareas);
+    iTareaAModificar = listaTareas.indexOf(tareaAModificar);
+    if (iTareaAModificar === -1) {
+        alert("ERROR: La tarea ingresada no existe.");
+    }
+    else {
+        tareaModificada = prompt("¿Cuál será el nuevo nombre de la tarea seleccionada?");
+        if (tareaModificada.trim() !== tareaModificada) {
+            alert("ERROR: Evita los espacios al inicio o al final de la tarea.");
+        }
+        else if (tareaModificada === "" || tareaModificada === null) {
+            alert("ERROR:: No puede ingresar un espacio vacío.");
+        }
+        else {
+            listaTareas.splice(iTareaAModificar, 1, tareaModificada);
+            alert("Se modificó correctamente la tarea.");
+        }
+    }
 }
 
 function tareaElim(tareaAEliminar, iTareaAEliminar) {
-    tareaAEliminar = prompt("¿Que tarea desea eliminar de la lista? " + listaTareas)
-    iTareaAEliminar = listaTareas.indexOf(tareaAEliminar)
-    listaTareas.splice(iTareaAEliminar, 1)
-    alert("Se elimino exitosamente la tarea")
+    tareaAEliminar = prompt("¿Qué tarea desea eliminar de la lista? " + listaTareas);
+    iTareaAEliminar = listaTareas.indexOf(tareaAEliminar);
+    if (iTareaAEliminar === -1) {
+        alert("ERROR: La tarea ingresada no existe.");
+    }
+    else {
+        listaTareas.splice(iTareaAEliminar, 1);
+        alert("Se eliminó exitosamente la tarea.");
+    }
 }
 
 do {
-    accionUsuario = parseInt(prompt("Seleccione que quiere hacer 1-Agregar Tarea 2-Listar Tareas 3-Editar Tarea 4-Eliminar Tarea  5-Salir"))
-    
+    accionUsuario = parseInt(prompt("Seleccione qué quiere hacer: 1-Agregar Tarea 2-Listar Tareas 3-Editar Tarea 4-Eliminar Tarea 5-Salir"));
+
     switch (accionUsuario) {
         case 1:
-            tareaAgr()
-        break;
+            tareaAgr();
+            break;
         case 2:
-            alert("Sus tareas guardadas son: " + listaTareas)
-        break;
+            alert("Sus tareas guardadas son: " + listaTareas);
+            break;
         case 3:
-            tareaEdit()
-        break;
+            tareaEdit();
+            break;
         case 4:
-            tareaElim()
-        break;
+            tareaElim();
+            break;
         case 5:
-            alert("Usted ha salido")
-        break;
+            alert("Usted salió del programa.");
+            break;
         default:
-            alert("Valor no valido")      
-        break;
+            alert("ERROR: Valor no válido.");
+            break;
     }
-} while (accionUsuario !== 5)
-
-
-    
+} while (accionUsuario !== 5);
